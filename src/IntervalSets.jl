08fc04af3ec61d2b8016935245ca37c43e42262a
Tim Holy <tim.holy@gmail.com>
2017-03-12 08:33:22 -0500

    Convert Ranges to Intervals

--- src/IntervalSets.jl
+++ src/IntervalSets.jl
@@ -23,6 +23,10 @@ function convert{I<:AbstractInterval}(::Type{I}, i::AbstractInterval)
     T = eltype(I)
     I(convert(T, i.left), convert(T, i.right))
 end
+function convert{I<:AbstractInterval}(::Type{I}, r::Range)
+    T = eltype(I)
+    I(convert(T, minimum(r)), convert(T, maximum(r)))
+end
 
 ordered{T}(a::T, b::T) = ifelse(a < b, (a, b), (b, a))
 ordered(a, b) = ordered(promote(a, b)...)
