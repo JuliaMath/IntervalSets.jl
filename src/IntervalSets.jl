43d2d95a6f3ca2f946c67b2d14172087b0ad54f6
Sheehan Olver <solver@mac.com>
2018-03-19 21:05:38 +0000

    Remove dodgy convert routine

--- src/IntervalSets.jl
+++ src/IntervalSets.jl
@@ -71,10 +71,6 @@ eltype(::Type{AbstractInterval{T}}) where {T} = T
 @pure eltype(::Type{I}) where {I<:AbstractInterval} = eltype(supertype(I))
 
 convert(::Type{I}, i::I) where {I<:AbstractInterval} = i
-function convert(::Type{I}, r::AbstractRange) where I<:AbstractInterval
-    T = eltype(I)
-    I(convert(T, minimum(r)), convert(T, maximum(r)))
-end
 
 ordered(a::T, b::T) where {T} = ifelse(a < b, (a, b), (b, a))
 ordered(a, b) = ordered(promote(a, b)...)
