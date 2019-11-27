ed05e67bf169888c101431f5730ef564abfc3cbd
Michael Abbott <me@escbook>
2019-11-27 15:25:40 +0100

    use .. from EllipsisNotation.jl

--- src/interval.jl
+++ src/interval.jl
@@ -81,8 +81,13 @@ convert(::Type{TypedEndpointsInterval{L,R}}, d::Interval{L,R}) where {L,R} = d
     iv = l..r
 
 Construct a ClosedInterval `iv` spanning the region from `l` to `r`.
+
+(The symbol `..` is the same as in the package EllipsisNotation.jl.)
 """
-..(x, y) = ClosedInterval(x, y)
+..
+
+(::Ellipsis)(x, y) = ClosedInterval(x, y)
+
 
 """
     iv = center±halfwidth
