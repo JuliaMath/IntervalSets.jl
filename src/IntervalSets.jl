689d68656757f7d5c0fa875f5f5542674360a6a4
hyrodium <hyrodium@gmail.com>
2020-04-11 17:45:23 +0900

    use Base.depwarn instead of @deprecate

--- src/IntervalSets.jl
+++ src/IntervalSets.jl
@@ -131,15 +131,29 @@ in(::Missing, I::TypedEndpointsInterval{:open,:open}) = !isempty(I) && missing
 in(::Missing, I::TypedEndpointsInterval{:closed,:open}) = !isempty(I) && missing
 in(::Missing, I::TypedEndpointsInterval{:open,:closed}) = !isempty(I) && missing
 
-@deprecate in(a::AbstractInterval,                         b::TypedEndpointsInterval{:closed,:closed}) (leftendpoint(a) ≥ leftendpoint(b)) & (rightendpoint(a) ≤ rightendpoint(b))
-@deprecate in(a::TypedEndpointsInterval{:open,:open},      b::TypedEndpointsInterval{:open,:open}    ) (leftendpoint(a) ≥ leftendpoint(b)) & (rightendpoint(a) ≤ rightendpoint(b))
-@deprecate in(a::TypedEndpointsInterval{:closed,:open},    b::TypedEndpointsInterval{:open,:open}    ) (leftendpoint(a) > leftendpoint(b)) & (rightendpoint(a) ≤ rightendpoint(b))
-@deprecate in(a::TypedEndpointsInterval{:open,:closed},    b::TypedEndpointsInterval{:open,:open}    ) (leftendpoint(a) ≥ leftendpoint(b)) & (rightendpoint(a) < rightendpoint(b))
-@deprecate in(a::TypedEndpointsInterval{:closed,:closed},  b::TypedEndpointsInterval{:open,:open}    ) (leftendpoint(a) > leftendpoint(b)) & (rightendpoint(a) < rightendpoint(b))
-@deprecate in(a::TypedEndpointsInterval{:closed},          b::TypedEndpointsInterval{:open,:closed}  ) (leftendpoint(a) > leftendpoint(b)) & (rightendpoint(a) ≤ rightendpoint(b))
-@deprecate in(a::TypedEndpointsInterval{:open},            b::TypedEndpointsInterval{:open,:closed}  ) (leftendpoint(a) ≥ leftendpoint(b)) & (rightendpoint(a) ≤ rightendpoint(b))
-@deprecate in(a::TypedEndpointsInterval{L,:closed}, b::TypedEndpointsInterval{:closed,:open}) where L  (leftendpoint(a) ≥ leftendpoint(b)) & (rightendpoint(a) < rightendpoint(b))
-@deprecate in(a::TypedEndpointsInterval{L,:open},   b::TypedEndpointsInterval{:closed,:open}) where L  (leftendpoint(a) ≥ leftendpoint(b)) & (rightendpoint(a) ≤ rightendpoint(b))
+function in(a::AbstractInterval, b::AbstractInterval)
+    Base.depwarn("`in(a, b)` (equivalently, `a ∈ b`) is deprecated in favor of `issubset(a, b)` (equivalently, `a ⊆ b`). Note that the behavior for empty intervals is also changing.", :in)
+    return in_deprecation(a, b)
+end
+
+in_deprecation(a::AbstractInterval,                         b::TypedEndpointsInterval{:closed,:closed}) =
+    (leftendpoint(a) ≥ leftendpoint(b)) & (rightendpoint(a) ≤ rightendpoint(b))
+in_deprecation(a::TypedEndpointsInterval{:open,:open},      b::TypedEndpointsInterval{:open,:open}    ) =
+    (leftendpoint(a) ≥ leftendpoint(b)) & (rightendpoint(a) ≤ rightendpoint(b))
+in_deprecation(a::TypedEndpointsInterval{:closed,:open},    b::TypedEndpointsInterval{:open,:open}    ) =
+    (leftendpoint(a) > leftendpoint(b)) & (rightendpoint(a) ≤ rightendpoint(b))
+in_deprecation(a::TypedEndpointsInterval{:open,:closed},    b::TypedEndpointsInterval{:open,:open}    ) =
+    (leftendpoint(a) ≥ leftendpoint(b)) & (rightendpoint(a) < rightendpoint(b))
+in_deprecation(a::TypedEndpointsInterval{:closed,:closed},  b::TypedEndpointsInterval{:open,:open}    ) =
+    (leftendpoint(a) > leftendpoint(b)) & (rightendpoint(a) < rightendpoint(b))
+in_deprecation(a::TypedEndpointsInterval{:closed},          b::TypedEndpointsInterval{:open,:closed}  ) =
+    (leftendpoint(a) > leftendpoint(b)) & (rightendpoint(a) ≤ rightendpoint(b))
+in_deprecation(a::TypedEndpointsInterval{:open},            b::TypedEndpointsInterval{:open,:closed}  ) =
+    (leftendpoint(a) ≥ leftendpoint(b)) & (rightendpoint(a) ≤ rightendpoint(b))
+in_deprecation(a::TypedEndpointsInterval{L,:closed}, b::TypedEndpointsInterval{:closed,:open}) where L  =
+    (leftendpoint(a) ≥ leftendpoint(b)) & (rightendpoint(a) < rightendpoint(b))
+in_deprecation(a::TypedEndpointsInterval{L,:open},   b::TypedEndpointsInterval{:closed,:open}) where L  =
+    (leftendpoint(a) ≥ leftendpoint(b)) & (rightendpoint(a) ≤ rightendpoint(b))
 
 isempty(A::TypedEndpointsInterval{:closed,:closed}) = leftendpoint(A) > rightendpoint(A)
 isempty(A::TypedEndpointsInterval) = leftendpoint(A) ≥ rightendpoint(A)
@@ -153,22 +167,22 @@ isequal(A::TypedEndpointsInterval, B::TypedEndpointsInterval) = isempty(A) & ise
 function issubset(A::TypedEndpointsInterval, B::TypedEndpointsInterval)
     Al, Ar = endpoints(A)
     Bl, Br = endpoints(B)
-    return isempty(A) | ( Bl ≤ Al && Ar ≤ Br )
+    return isempty(A) | ( Bl ≤ Al & Ar ≤ Br )
 end
 function issubset(A::TypedEndpointsInterval{:closed,R1} where R1, B::TypedEndpointsInterval{:open,R2} where R2)
     Al, Ar = endpoints(A)
     Bl, Br = endpoints(B)
-    return isempty(A) || ( Bl < Al && Ar ≤ Br )
+    return isempty(A) | ( Bl < Al & Ar ≤ Br )
 end
 function issubset(A::TypedEndpointsInterval{L1,:closed} where L1, B::TypedEndpointsInterval{L2,:open} where L2)
     Al, Ar = endpoints(A)
     Bl, Br = endpoints(B)
-    return isempty(A) || ( Bl ≤ Al && Ar < Br )
+    return isempty(A) | ( Bl ≤ Al & Ar < Br )
 end
 function issubset(A::TypedEndpointsInterval{:closed,:closed}, B::TypedEndpointsInterval{:open,:open})
     Al, Ar = endpoints(A)
     Bl, Br = endpoints(B)
-    return isempty(A) || ( Bl < Al && Ar < Br )
+    return isempty(A) | ( Bl < Al & Ar < Br )
 end
 
 ⊇(A::AbstractInterval, B::AbstractInterval) = issubset(B, A)
