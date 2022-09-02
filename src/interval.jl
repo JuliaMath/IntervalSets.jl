ba972e55ec2b7bc4566cf81780e0de2b16142db3
Zachary Sunberg <zachary.sunberg@colorado.edu>
2022-09-01 18:10:44 -0600

    added rand for TypedEndpointsInterval

--- src/interval.jl
+++ src/interval.jl
@@ -222,11 +222,11 @@ end
 
 # random sampling from interval
 Random.gentype(::Type{Interval{L,R,T}}) where {L,R,T} = float(T)
-function Random.rand(rng::AbstractRNG, i::Random.SamplerTrivial{ClosedInterval{T}}) where {T<:Real}
+function Random.rand(rng::AbstractRNG, i::Random.SamplerTrivial{<:TypedEndpointsInterval{:closed, R, T}}) where {R, T<:Real}
     _i = i[]
     isempty(_i) && throw(ArgumentError("The interval should be non-empty."))
     a,b = endpoints(_i)
-    t = rand(rng, float(T))
+    t = rand(rng, float(T)) # technically this samples from [0, 1), but we still allow it with TypedEndpointsInterval{:closed, :closed} for convenience
     return clamp(t*a+(1-t)*b, _i)
 end
 
